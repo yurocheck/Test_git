4399771a94b6c776752b7ddd3780fcae5e15f2e9
Yurii Kulagin <yuk.178@bk.ru>
2026-03-18 18:24:16 +0300

    KOT-3-1 array

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,62 @@
 package org.example
 
+const val AGE_OF_MAJORITY = 18
+const val RETIREMENT_AGE = 65
+
 fun main() {
-        println("Hello, Git!")
+        val c = ("Hello, Git!")
+        println(c::class.simpleName)
+        println(c)
+
+        println()
+        println()
+
+
+        val userAge = readln().toInt()
+
+        val resultText = if (userAge >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
+        else if (userAge == 16 || userAge == 17) "Показать экран с ограниченным контентом"
+        else "Вернуться на главный экран приложения"
+        println(resultText)
+
+        println()
+        println()
+
+        var counter = 5
+        while (counter > 0) {
+                println("Реклама закончится через ${counter--}")
+                Thread.sleep(1000)
+        }
+
+        println()
+        println()
+
+        for (i in 5 downTo 1) {
+                println("Реклама закончится через $i")
+                Thread.sleep(1000)
+
+        }
+
+        println()
+        println()
+
+
+        for (i in 5 downTo 1) {
+                if (i == 3) {
+                        println("Пользователь нажал на кнопку \"Пропустить\"")
+                        break
+                }
+                println("Реклама закончится через $i")
+                Thread.sleep(1000)
+        }
+        println("Продолжение работы программы вне цикла")
+
+        println()
+        println()
+
+        val arrayOfIngredients = arrayOf("яйцо", "помидор", "зелень", "соль", "перец")
+        for (i in arrayOfIngredients) {
+                println("Ингредиент ${arrayOfIngredients.indexOf(i) + 1}: $i")
+        }
+
 }
